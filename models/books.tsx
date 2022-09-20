c4f9eff6602b73b190c3d3c67515e85c5fd7e910
Hamza Eşsiz <webcatdev@gmail.com>
2022-09-20 16:35:18 +0300

    removed port from sequelize

--- models/books.tsx
+++ models/books.tsx
@@ -2,7 +2,6 @@ import { Sequelize, DataTypes } from 'sequelize';
 
 const sequelize = new Sequelize('q1XCMFzfMX', 'q1XCMFzfMX', 'VndKoxkhro', {
 	host: 'remotemysql.com',
-	port: 3306,
 	dialect: 'mysql',
 });
 
