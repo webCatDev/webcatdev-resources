5466798a3767a4dd076cdba62af3d07e528cc15f
Hamza Eşsiz <webcatdev@gmail.com>
2022-09-21 11:28:52 +0300

    api check

--- pages/api/books/index.tsx
+++ pages/api/books/index.tsx
@@ -7,28 +7,29 @@ export default async function handler(
 	request: NextApiRequest,
 	response: NextApiResponse
 ) {
+
+	response.end('hello world')
 	
-	try {
-		await Book.sync()
-		if (request.method === 'GET') {
-			console.log('home page .....')
-			const books = await Book.findAll({attributes: ['id','title', 'cover']})
-			response.json(books)
-		}
+	// try {
+	// 	await Book.sync()
+	// 	if (request.method === 'GET') {
+	// 		const books = await Book.findAll({attributes: ['id','title', 'cover']})
+	// 		response.json(books)
+	// 	}
 
-		if (request.method === 'POST') {
-			const { title, description, author, cover } = request.body			
-			const book = await Book.create({ title, description, author, cover });
-			response.json(book)
-		}
+	// 	if (request.method === 'POST') {
+	// 		const { title, description, author, cover } = request.body			
+	// 		const book = await Book.create({ title, description, author, cover });
+	// 		response.json(book)
+	// 	}
 
-	} catch (err) {
-		response.statusCode = 500;
-		response.json({
-			status: 'failed',
-			statusCode: response.statusCode,
-			message: err.message,
-			error: err
-		});
-	}
+	// } catch (err) {
+	// 	response.statusCode = 500;
+	// 	response.json({
+	// 		status: 'failed',
+	// 		statusCode: response.statusCode,
+	// 		message: err.message,
+	// 		error: err
+	// 	});
+	// }
 }
