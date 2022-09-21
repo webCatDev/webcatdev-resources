1771a691c5bfbc20ec9a9e4fa00b2a0e41697c54
Hamza Eşsiz <webcatdev@gmail.com>
2022-09-21 11:34:16 +0300

    api check

--- pages/api/books/index.tsx
+++ pages/api/books/index.tsx
@@ -6,30 +6,27 @@ import { Book } from '../../../models/books';
 export default async function handler(
 	request: NextApiRequest,
 	response: NextApiResponse
-) {
+) {	
+	try {
+		await Book.sync({force: true})
+		if (request.method === 'GET') {
+			const books = await Book.findAll({attributes: ['id','title', 'cover']})
+			response.json(books)
+		}
 
-	response.json([])
-	
-	// try {
-	// 	await Book.sync()
-	// 	if (request.method === 'GET') {
-	// 		const books = await Book.findAll({attributes: ['id','title', 'cover']})
-	// 		response.json(books)
-	// 	}
+		if (request.method === 'POST') {
+			const { title, description, author, cover } = request.body			
+			const book = await Book.create({ title, description, author, cover });
+			response.json(book)
+		}
 
-	// 	if (request.method === 'POST') {
-	// 		const { title, description, author, cover } = request.body			
-	// 		const book = await Book.create({ title, description, author, cover });
-	// 		response.json(book)
-	// 	}
-
-	// } catch (err) {
-	// 	response.statusCode = 500;
-	// 	response.json({
-	// 		status: 'failed',
-	// 		statusCode: response.statusCode,
-	// 		message: err.message,
-	// 		error: err
-	// 	});
-	// }
+	} catch (err) {
+		response.statusCode = 500;
+		response.json({
+			status: 'failed',
+			statusCode: response.statusCode,
+			message: err.message,
+			error: err
+		});
+	}
 }
