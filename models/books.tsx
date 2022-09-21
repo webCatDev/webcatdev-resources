898724f30cb4f8c2c756f3c31da657fca5c7101d
Hamza Eşsiz <webcatdev@gmail.com>
2022-09-21 13:12:40 +0300

    connected to railway mysql

--- models/books.tsx
+++ models/books.tsx
@@ -1,9 +1,13 @@
 import { Sequelize, DataTypes } from 'sequelize';
 
-const sequelize = new Sequelize('q1XCMFzfMX', 'q1XCMFzfMX', 'VndKoxkhro', {
-	host: 'remotemysql.com',
-	dialect: 'mysql',
-});
+// const sequelize = new Sequelize('q1XCMFzfMX', 'q1XCMFzfMX', 'VndKoxkhro', {
+// 	host: 'remotemysql.com',
+// 	dialect: 'mysql',
+// });
+
+const sequelize = new Sequelize(
+	'mysql://root:uqixmW2KXdfd5IE5TO1K@containers-us-west-37.railway.app:6422/railway'
+);
 
 export const Book = sequelize.define('Book', {
 	id: {
